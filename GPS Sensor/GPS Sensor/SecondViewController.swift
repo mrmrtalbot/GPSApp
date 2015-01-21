8fd254eb9ca1cab2e1c59a455f93e3257b0aed63
mrmrtalbot <mrmrtalbot@googlemail.com>
2015-01-21 13:56:31 +0000

    First Commit

---
+++ GPS Sensor/GPS Sensor/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  GPS Sensor
+//
+//  Created by Martin on 15/01/2015.
+//  Copyright (c) 2015 Martin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
